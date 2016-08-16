69a7fe25ba7f35b574cbc276e781e83d95d14b8f
Tsai chen ping <oscar60310@gmail.com>
2016-08-17 02:02:22 +0800

    twitch server

---
+++ irc/twitch.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import socket 
+import time
+import threading
+class twitch:
+  def __init__(self,cfg):
+    self.cfg = cfg
+    self.active = True 
+    self.timer = 0
+    self.msg_sended = 0
+    self.botname = cfg['bot_name']
+  def msg(self,m):
+    print '%s [Twitch] %s' % (time.strftime("%b %d %Y %H:%M:%S"),m)
+  def connect(self):
+    #try:
+      self.msg('try to connect twitch server.')
+      self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.client.connect(("irc.chat.twitch.tv", 6667))
+      self.msg('twitch server connected.')
+      # start receive tread
+      self.recv_thread = threading.Thread(target = self.recv_func)
+      self.recv_thread.daemon = True 
+      self.recv_thread.start()
+      # start send msg timer
+      self.timer_thread = threading.Thread(target = self.limit_time)
+      self.timer_thread.daemon = True 
+      self.timer_thread.start()
+      # login
+      self.send('PASS ' + self.cfg['bot_pass'] + '\n')
+      self.send('NICK ' + self.cfg['bot_name'] + '\n')
+      self.send('JOIN #'+ self.cfg['bot_name'] + '\n')
+      # Welcome msg
+      self.send_to_room(self.botname,'Bot service connected.')
+      return True
+    #except:
+     # return False
+  def start(self):
+    while not self.connect() and self.active:
+      self.msg('connect fail, try again in 5 seconds.')
+      time.sleep(5)
+  def stop(self):
+    self.active = False
+    self.msg('shut down connect between twitch server...')
+    self.client.shutdown(socket.SHUT_RDWR)
+    self.client.close()
+  def recv(self):
+    tmp = ""
+    chunk = self.client.recv(225) 
+    tmp += chunk
+    if tmp.find('\n') != -1: 
+      ss = tmp.split('\n')
+      for ns in range(len(ss)-1):
+        self.msg(ss[ns][0:len(ss[ns])-1])
+      tmp = ss[len(ss)-1]
+  def recv_func(self):
+    self.msg('start receive message.')
+    while self.active:
+      self.recv()
+    self.msg('stop receive message.')
+  def limit_time(self):
+    self.msg('start message timer.')
+    while self.active:
+      self.timer += 1
+      if self.timer >= 30:
+        self.msg_sended = 0
+        self.timer = 0
+      time.sleep(1)
+    self.msg('stop message timer.')
+  def send(self,msgs):
+    while self.msg_sended > 15:
+      self.msg('too many message, waitting...')
+      time.sleep(1)
+    self.msg_sended += 1
+    self.client.sendall(msgs)
+    #self.msg(' [send] ' + msgs)
+  def handle_msg(self,msgs):
+    if msgs == "PING :tmi.twitch.tv":
+      self.msg('respond ping request.')
+      self.send('PONG :tmi.twitch.tv\n')
+  def send_to_room(self,room,msgs):
+    self.send('PRIVMSG #{0} :{1}\n'.format(room,msgs))
+
+      
--- main.py
+++ main.py
@@ -6,25 +6,12 @@ import json
 cfg = json.loads(config_file.read())
 config_file.close()
 
-#from pymongo import MongoClient
-#client = MongoClient(cfg['db_ip'],cfg['db_port']).bot
-#client.authenticate(cfg['db_user'],cfg['db_pass'])
-
-#for user in client.user.find():
-#  print user['name'] 
-
-# web socket server
-from server import serverControl
-server = serverControl(cfg)
-def server_go():
-  server.start_server()
-# start web socket
-import threading
-server_thread = threading.Thread(target = server_go)
-server_thread.daemon = True 
-server_thread.start()
+from irc.twitch import twitch
+twitch = twitch(cfg)
+# start twitch connect
+twitch.start()
 while True:
   com = raw_input("")
   if com == 'q':
-    server.stop_server()
+    twitch.stop()
     break
